4e5aee4545d3b732197ec922f88bf665450a9063
realAfoke <ighoafokereality3@gmail.com>
2026-08-18 16:29:29 +0100

    created notification url file and designed notification model migrated the model

--- backend/notification/models.py
+++ backend/notification/models.py
@@ -1,3 +1,27 @@
+from django.core.checks import messages
 from django.db import models
+from workspace.models import Membership,WorkSpace,Task
 
 # Create your models here.
+#
+#
+
+class Notification(models.Model):
+    title=models.CharField(max_length=500)
+    user=models.ForeignKey(Membership,related_name='nofication',on_delete=models.CASCADE)
+    workspace=models.ForeignKey(WorkSpace,related_name="workspace_notification",on_delete=models.CASCADE)
+    type=models.CharField(max_length=250)
+    task=models.ForeignKey(Task,related_name='task_notification',on_delete=models.SET_NULL,null=True,blank=True)
+    related_user=models.ForeignKey(Membership,related_name='related_notification',on_delete=models.SET_NULL,null=True,blank=True)
+    message=models.TextField()
+    read=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        db_table='notification'
+
+    def __str__(self):
+        return self.title
+
+

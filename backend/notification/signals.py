b4db64092f7c8c135e65f47548e4c2e912bc56dc
realAfoke <ighoafokereality3@gmail.com>
2026-08-20 16:32:55 +0100

    feat:added comment notification signal

--- backend/notification/signals.py
+++ backend/notification/signals.py
@@ -1,6 +1,6 @@
 from django.dispatch import receiver
 import manage
-from workspace.models import Task
+from workspace.models import Task,Comment
 from notification.models import Notification
 from django.db.models.signals import post_save
 from channels.layers import get_channel_layer
@@ -8,17 +8,14 @@ from notification.api.serializers import NotificationSerializer
 from asgiref.sync import async_to_sync
 
 
-
-@receiver(post_save,sender=Task,dispatch_uid='task_notification')
-def send_task_nofitication(sender,instance,created,**kwargs):
+def send_notification(members,instance):
     channel=get_channel_layer()
-    members=instance.task_member.all()
     manager=getattr(Notification,'objects')
     notification=manager.create(
             title=f'{instance.created_by} assigned you to a task',
             workspace=instance.workspace,
             type='assignment',
-            message=instance.description,
+            message=getattr(instance,'description',getattr(instance,'content',None)),
             task=instance,
 
             )
@@ -27,5 +24,15 @@ def send_task_nofitication(sender,instance,created,**kwargs):
     for member in members:
         async_to_sync(channel.group_send)(f'member_{member.user.user.id}',{'type':'send_notification','notification':serializer.data})
 
+
+@receiver(post_save,sender=Task,dispatch_uid='task_notification')
+def send_task_nofitication(sender,instance,created,**kwargs):
+    members=instance.task_member.all()
+    send_notification(members,instance)
+    
+@receiver(post_save,sender=Comment,dispatch_uid='comment_notificaiton')
+def send_comment_notification(send,instance,created,**kwargs):
+    members=instance.task.task_member.all()
+    send_notification(members,instance)
 
 
